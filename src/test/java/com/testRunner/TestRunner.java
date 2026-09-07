276fe4d376861291b4f2e199fd7e61d47866012c
Bharathkumar <bharathkumar.kiaq@gmail.com>
2026-09-07 16:04:33 +0530

    Final Commit

--- src/test/java/com/testRunner/TestRunner.java
+++ src/test/java/com/testRunner/TestRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "classpath:com.feature", glue = { "com.stepDefinitions",
+@CucumberOptions(features = "classpath:com.feature/Login.feature", glue = { "com.stepDefinitions",
 		"com.hooksClass" }, plugin = { "pretty", "summary", "html:target/CucumberReport.html",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
@@ -21,9 +21,9 @@ public class TestRunner extends AbstractTestNGCucumberTests {
 //		BrowserContext.setBrowser(browser);
 //	}
 
-	@Override
-	@DataProvider(parallel = true)
-	public Object[][] scenarios() {
-		return super.scenarios();
-	}
+//	@Override
+//	@DataProvider(parallel = true)
+//	public Object[][] scenarios() {
+//		return super.scenarios();
+//	}
 }
